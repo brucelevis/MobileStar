b8b348d0acf533fdbd2a9c8dafaa9112702e0fed
Bangu <doug0476@naver.com>
2015-11-18 08:16:10 +0900

    Android.mk 페이스북 연동 삭제

--- Client/MobileStar/proj.android/jni/Android.mk
+++ Client/MobileStar/proj.android/jni/Android.mk
@@ -17,7 +17,6 @@ LOCAL_SRC_FILES += hellocpp/main.cpp
 
 LOCAL_LDLIBS := -landroid -llog
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
-LOCAL_WHOLE_STATIC_LIBRARIES := PluginFacebook sdkbox
 
 # _COCOS_HEADER_ANDROID_BEGIN
 # _COCOS_HEADER_ANDROID_END
@@ -31,8 +30,6 @@ LOCAL_STATIC_LIBRARIES := cocos2dx_static
 include $(BUILD_SHARED_LIBRARY)
 
 $(call import-module,.)
-$(call import-module, ./sdkbox)
-$(call import-module, ./pluginfacebook)
 
 # _COCOS_LIB_IMPORT_ANDROID_BEGIN
 # _COCOS_LIB_IMPORT_ANDROID_END
